5342700e4c866b722f4af68551ad317953e62303
Anzel <anzelnieuwenhuizen@gmail.com>
2020-04-27 13:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-if ( string === string.toLowerCase) {
+if ( 'hello' === string.toLowerCase) {
   return ("I can\'t hear you!")
 }
 }
